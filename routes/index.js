08acb051c027df7983b3ab40bff79e7a6eb2c51b
Justin Pinero <justinpinero@gmail.com>
2020-07-24 13:25:41 -0400

    heroku deployment scripts

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const employeeRoutes = require("./employee");
 
 router.use('/api', employeeRoutes);
-router.get('/', (req, res) => res.send('I AM ROOT'));
 
 module.exports = router
